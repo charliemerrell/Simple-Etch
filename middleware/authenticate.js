b2b4a40d8eafda04a751fcfc1e918821823a2f12
Charlie Merrell <charlie.merrell20@gmail.com>
2022-03-05 13:22:37 +0000

    remove db from git

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,11 +1,14 @@
 const { Buffer } = require("buffer");
 
 function authenticate(req, res, next) {
-    const authHeader = req.get("Authorization");
     if (authHeader) {
-        const password = Buffer.from(authHeader?.split(" ")[1], "base64");
+        const password = Buffer.from(
+            authHeader?.split(" ")[1],
+            "base64"
+        ).toString();
+        console.log(password);
         if (password === process.env.PASSWORD) {
-            next();
+            return next();
         }
     }
     res.sendStatus(403);
