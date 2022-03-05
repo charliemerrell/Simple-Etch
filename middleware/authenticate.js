f59558a181877760b8315aa6da66bb1f4e775ae6
Charlie Merrell <charlie.merrell20@gmail.com>
2022-03-05 14:22:15 +0000

    fix missing authHeader init

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,7 @@
 const { Buffer } = require("buffer");
 
 function authenticate(req, res, next) {
+    const authHeader = req.get("Authorization");
     if (authHeader) {
         const password = Buffer.from(
             authHeader?.split(" ")[1],
