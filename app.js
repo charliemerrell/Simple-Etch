fa0bddc22519fef3aef3d49399d88358b49f00f1
Charlie Merrell <charlie.merrell20@gmail.com>
2022-03-05 14:15:19 +0000

    Cors

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const path = require("path");
 const authenticate = require("./middleware/authenticate");
 require("dotenv").config();
-const db = require("./db");
 const initialiseDb = require("./initialiseDb");
 
 initialiseDb();
-
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+    const cors = require("cors");
+    const corsOptions = {
+        origin: process.env.CLIENT_URL,
+        optionsSuccessStatus: 200,
+    };
+    app.use(cors(corsOptions));
+}
 app.use(express.json());
 
 app.post("/api/login", (req, res) => {
     res.send("hello");
 });
+
 app.get("/api/cards", authenticate, (req, res) => {
     res.send("lots of cards");
 });
