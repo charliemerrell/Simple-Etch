f8759b3ea7fe34eced21ef44c4344b03f0524288
Charlie Merrell <charlie.merrell20@gmail.com>
2022-03-06 10:21:10 +0000

    added additional seed data

--- seedDbForDev.js
+++ seedDbForDev.js
@@ -1,6 +1,22 @@
 const initialiseDb = require("./initialiseDb");
 const { PROGRESS_FOR_COMPLETION } = require("./cardUtils");
 
+const randomCode = [
+    "function coinChange(target, coinsArr, iStart = 0) {",
+    "   if (target === 0) {",
+    "       return 1;",
+    "   }",
+    "   if (target < 0) {",
+    "       return 0;",
+    "   }",
+    "   let ways = 0;",
+    "   for (let i = iStart; i < coinsArr.length; i++) {",
+    "       ways += coinChange(target - coinsArr[i], coinsArr, i);",
+    "   }",
+    "   return ways;",
+    "}",
+].join("\n");
+
 function seedDbForDev(db) {
     console.log("seeding for dev");
     db.serialize(() => {
@@ -9,6 +25,10 @@ function seedDbForDev(db) {
         db.run(
             "INSERT INTO Cards(question, answer, progress, ripe) VALUES(?, ?, ?, ?)",
             ["Ripe and progress 0", "Just so you know", 0, Date.now() - 1000]
+        );
+        db.run(
+            "INSERT INTO Cards(question, answer, progress, ripe) VALUES(?, ?, ?, ?)",
+            [randomCode, randomCode, 0, Date.now() - 1000]
         );
         db.run(
             "INSERT INTO Cards(question, answer, progress, ripe) VALUES(?, ?, ?, ?)",
